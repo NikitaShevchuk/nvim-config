49f85967f382b681863f6e2e165916ad0ce86cb3
Yehor Lautenshlager <flacomatsumoto@gmail.com>
2024-07-07 00:42:46 +0300

    init

---
+++ lua/config/options.lua
@@ -0,0 +1,13 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- function ColorMyPencil(color)
+--   color = color or "rose-pine-moon"
+--   vim.cmd.colorscheme(color)
+--
+--   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- end
+--
+-- ColorMyPencil()
